91db97111511a9c0f1785e734cc40129e9a01e92
Robert Ruder <bosesbjorn@gmail.com>
2020-04-24 21:54:30 -0500

    Worked with forms

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,15 @@
 class HomeController < ApplicationController
   def index
     @api = StockQuote::Stock.new(api_key: "pk_42833966e96f49a9bb23e12ed149e6aa")
+    if params[:ticker] == ""
+      @nothing = "Hey you forgot to enter a symbol."
+    elsif params[:ticker]
+      begin
+        @stock = StockQuote::Stock.quote(params[:ticker])
+      rescue RuntimeError => @errorMsg
+        @error = "That company symbol does not exist."
+      end
+    end
   end
 
   def about
