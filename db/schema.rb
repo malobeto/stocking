3eb049ad70fae41149fd7a7f0557fa6eed44d670
Robert Ruder <bosesbjorn@gmail.com>
2020-05-03 14:01:59 -0500

    Added accociations and played around with stocks and user databases

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200427205654) do
+ActiveRecord::Schema.define(version: 20200430205025) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "ticker"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_stocks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
